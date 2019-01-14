0d315385fe281dbc210e2f0d96692db2f5a38ae4
ermu0420 <992124332@qq.com>
2019-01-14 11:57:43 +0800

    init

---
+++ src/main/java/com/ermu/generator/StartUp.java
@@ -0,0 +1,49 @@
+package com.ermu.generator;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.mybatis.generator.api.MyBatisGenerator;
+import org.mybatis.generator.config.Configuration;
+import org.mybatis.generator.config.xml.ConfigurationParser;
+import org.mybatis.generator.exception.InvalidConfigurationException;
+import org.mybatis.generator.exception.XMLParserException;
+import org.mybatis.generator.internal.DefaultShellCallback;
+
+
+/**
+ * @author：xusonglin
+ * ===============================
+ * Created with IDEA.
+ * Date：18-11-5
+ * Time：下午2:27
+ * ================================
+ */
+public class StartUp {
+    public static void main(String[] args)  {
+        try {
+            List<String> warnings = new ArrayList<>();
+            ClassLoader classloader = Thread.currentThread().getContextClassLoader();
+            InputStream is = classloader.getResourceAsStream("generatorConfig.xml");
+            ConfigurationParser cp = new ConfigurationParser(warnings);
+            Configuration config = cp.parseConfiguration(is);
+            DefaultShellCallback callback = new DefaultShellCallback(true);
+            MyBatisGenerator myBatisGenerator = new MyBatisGenerator(config, callback, warnings);
+            myBatisGenerator.generate(null);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        } catch (XMLParserException e) {
+            e.printStackTrace();
+        }
+    }
+}
